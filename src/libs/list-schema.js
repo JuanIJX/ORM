ee148779e9edcde5b9d1738c59ebeb22ba62d867
JuanIJX <srwe-@hotmail.com>
2023-08-09 13:28:12 +0200

    Finish list-schema

--- src/libs/list-schema.js
+++ src/libs/list-schema.js
@@ -1,4 +1,5 @@
 import { Schema } from "./schema.js";
+import { Cmp, Where } from "./where.js";
 
 export default class List {
 	constructor(objectEntity, entity) {
@@ -9,24 +10,43 @@ export default class List {
 
 		Object.defineProperty(this, `_obj`, { value: objectEntity });
 		Object.defineProperty(this, `_entity`, { value: entity });
+		Object.defineProperty(this, `_conn`, { value: entity.connector });
 
-		Object.defineProperty(this, `id`, { value: this._obj.pk, enumerable: true });
+		Object.defineProperty(this, `id`, { value: this._obj._id, enumerable: true });
+		Object.defineProperty(this, `size`, { get: async function() {
+			return await this._conn.count(Cmp.EQ(this._obj.constructor.fgName(), this._obj._id));
+		}, enumerable: true });
 	}
 
-	async get(id) { return null; }
-	async getAll(index=0, limit=null) {
-
+	async get(id, ...where) {
+		const dataDB = (await this._conn.getElements(null, Where.AND(
+			Cmp.EQ(this._entity.config.pkName, id),
+			Cmp.EQ(this._obj.constructor.fgName(), this._obj._id),
+			...where
+		)))[0];
+		if(!dataDB)
+			return null;
+		return this._entity._getObj(dataDB);
+	}
+	async getAll(limit=null, offset=0, where=null) {
+		where = [
+			Cmp.EQ(this._obj.constructor.fgName(), this._obj._id),
+			...(where === null ? [] : [where])
+		];
+		const objs = [];
+		const dataDB = await this._conn.getElements(null, Where.AND(...where), this._entity.config.createdAt ? ["created_at"] : [], true, limit, offset);
+		for (const userDB of dataDB)
+			objs.push(this._entity._getObj(userDB));
+		return objs;
 	}
 	async add(data) {
-
+		return await this._entity.add({ data, [this._obj.constructor.fgName()]: this._obj._id})
 	}
-	async delete() {
-
-	}
-	async deleteAll(index=0, limit=null) {
-
-	}
-	async getSize() {
-
+	async deleteAll(limit=null, offset=0, where=null) {
+		where = [
+			Cmp.EQ(this._obj.constructor.fgName(), this._obj._id),
+			...(where === null ? [] : [where])
+		];
+		await this._conn.deleteElements(Where.AND(...where), limit, offset);
 	}
 }
