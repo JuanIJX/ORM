516eed3b6ca160decd28626e99d28924990e6304
JuanIJX <srwe-@hotmail.com>
2023-08-14 00:03:00 +0200

    Fix procedure

--- src/db-connectors/mysql-conn.js
+++ src/db-connectors/mysql-conn.js
@@ -172,7 +172,7 @@ export class MysqlConnector extends DBConnector {
 		});
 		return obj;
 	};
-	static async procedure(name, ...vars) { return (await idbd.query(callSql(name, ...vars)))[0][0]; }
+	static async procedure(name, ...vars) { return (await this.idbd.query(callSql(name, ...vars)))[0][0]; }
 
 	// Abstract functions repository
 	static async getElements(table, selects=null, where=null, orders=[], order=true, limit=null, offset=0) {
