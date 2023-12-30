b2f8b36953bdd4b5fff42a1cbef7182c53987f6a
JuanIJX <srwe-@hotmail.com>
2023-12-30 03:34:29 +0100

    Add export DBConnector

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export const version = "tempVersion.0.0"
 export * from "./libs/orm.js"
 export * from "./libs/schema.js"
 export * from "./libs/where.js"
+export * from "./libs/conn.js"
 
 export * from "./types/connectors.js"
 export * from "./types/db-type.js"
